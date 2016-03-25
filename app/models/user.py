4cf3d4c9c6215e6481cc968433a404d12b2ead4a
ank2809 <anshkothary@gmail.com>
2016-03-25 15:16:01 -0400

    Reorganized more

---
+++ app/models/user.py
@@ -0,0 +1,14 @@
+from .. import db
+
+class User(db.Document):
+  name = db.StringField(required=True,unique=True)
+  password = db.StringField(required=True)
+  def is_authenticated(self):
+    users = User.objects(name=self.name, password=self.password)
+    return len(users) != 0
+  def is_active(self):
+    return True
+  def is_anonymous(self):
+    return False
+  def get_id(self):
+    return self.name
