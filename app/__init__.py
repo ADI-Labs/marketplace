805e613e353f1636e99719477c2597cd14f5cf2c
James Shin <James Shin>
2016-03-25 17:33:30 -0400

    Add photo functionality, updated format

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from werkzeug import secure_filename
 import requests
 import os
 
-UPLOAD_FOLDER = 'C:/Users/james/uploads/'
+UPLOAD_FOLDER = 'C:/Users/uploads/' # This must be changed to your directory
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'PNG', 'JPG', 'JPEG', 'GIF'])
 
 app = Flask(__name__)
@@ -95,25 +95,6 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
-
-@app.route('/uplooad/', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''   
 
 @app.route("/sell/",methods=["POST","GET"])
 def sell():
