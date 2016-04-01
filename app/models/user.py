610e4c2f7828895ae52501192ec859f1a3b3dae7
Quinn Torres <qmt2002@columbia.edu>
2016-04-01 17:56:32 -0400

    Added a contact info field to the user

--- app/models/user.py
+++ app/models/user.py
@@ -3,6 +3,7 @@ from .. import db
 class User(db.Document):
   name = db.StringField(required=True,unique=True)
   password = db.StringField(required=True)
+  contact_info = db.StringField(required=False)
   def is_authenticated(self):
     users = User.objects(name=self.name, password=self.password)
     return len(users) != 0
